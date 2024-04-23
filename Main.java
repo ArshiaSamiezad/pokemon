4982f3fd2a733ed31cab519bf1911f1d50921e6f
ArshiaSamiezad <arshiasamiezad@gmail.com>
2024-04-23 15:13:35 +0330

    putting energy still has bugs

--- Main.java
+++ Main.java
@@ -4,16 +4,16 @@ import view.AppView;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Usage 1: For Testing Code with a testcase
-        try {
-            MTester.initTestcaseConsumer("C:\\Users\\Sam\\IdeaProjects\\pokemon-master\\tests\\",
-                    "inputs1\\inputDasti.txt",
-                    "outputs1\\outputDasti.txt",
-                    "myOut\\myOut1.txt");
-        } catch (Exception e) {
-            // throw e;
-            // This occurs when code is judging in quera
-            // Therefore, There is no need to remove or comment this section of code when uploading.
-        }
+//        try {
+//            MTester.initTestcaseConsumer("C:\\Users\\Sam\\IdeaProjects\\pokemon-master\\tests\\",
+//                    "inputs1\\inputDasti.txt",
+//                    "outputs1\\outputDasti.txt",
+//                    "myOut\\myOut1.txt");
+//        } catch (Exception e) {
+//            // throw e;
+//            // This occurs when code is judging in quera
+//            // Therefore, There is no need to remove or comment this section of code when uploading.
+//        }
         (new AppView()).run();
     }
 }
